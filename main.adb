6f5aaa0704d8e5763864e77724a5ffb07631ae21
PiotrKedra <piotr.k9750@gmail.com>
2019-01-16 15:49:44 +0100

    created basic idea of how it works, its all in map.adb so run just it to test

--- main.adb
+++ main.adb
@@ -1,10 +1,26 @@
-with Gtk.Main, Gtk.Window;
+with Gtk.Main;
+with Gtk.Window; use Gtk.Window;
+with Gtk.Box;         use Gtk.Box;
+with Gtk.Image;
 
 procedure Main is
-   Window : Gtk.Window.Gtk_Window;
+   Window : Gtk_Window;
+   img : Gtk.Image.Gtk_Image;
+   hbox : gtk_hbox;
 begin
    Gtk.Main.Init;
-   Gtk.Window.Gtk_New (Window);
-   Gtk.Window.Show (Window);
+   
+   Gtk_New(Window);
+   Initialize(Window);
+   Set_Title(Window, "Froger");
+   Set_Default_Size(Window, 400, 400);
+   
+   Gtk_New_Hbox(hbox, False, 0);
+
+   Add(Window, hbox);
+   
+   Show(hbox);
+
+   Show(Window);
    Gtk.Main.Main;
 end Main;
