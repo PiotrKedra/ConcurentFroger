34c6574f68ccb1917ba365f0e6112da720bd414a
Milosz Blasiak <milblasiak@gmail.com>
2019-01-19 18:58:18 +0100

    Add Frog class empty frame

---
+++ Frog.adb
@@ -0,0 +1,23 @@
+procedure Frog is
+
+
+
+
+
+procedure body 
+protected type Frog is
+  
+  
+    procedure move;
+    procedure die;
+    
+private
+  y,x:Integer;
+  
+
+end Frog;
+
+protected body Frog is
+
+
+end Frog;
