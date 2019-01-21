8c2e1afebf047d7d1e8d3699cb1beb548e1abdf1
Milosz Blasiak <milblasiak@gmail.com>
2019-01-21 12:13:03 +0100

    Add simple menu function

---
+++ menu.adb
@@ -0,0 +1,64 @@
+package body menu is
+
+
+function show_menu return Integer is 
+current_selection:Integer:=1;
+keyCode : Character;
+WasPressed : Boolean;    
+no_exit:Boolean:=True;
+    begin
+        while no_exit loop
+            Ada.Text_IO.Get_Immediate (KeyCode, WasPressed);
+            if WasPressed then
+                if "" & KeyCode ="w" then
+                    current_selection:=current_selection-1;
+                end if;
+                if "" & KeyCode ="s" then
+                    current_selection:=current_selection+1;
+                end if;
+                if "" & KeyCode ="f" then
+                    current_selection:=current_selection-1;
+                end if;
+
+
+            end if;
+            WasPressed:=False;
+            new_line;
+            new_line;
+            new_line;
+            put_line("---------------------------");
+            put_line("-------F-R-O-G-E-R---------");
+            new_line;
+            
+            if current_selection=1 then
+                put_line("  -->   S-T-A-R-T          ");
+            else
+                put_line("        S-T-A-R-T          ");
+            end if;
+            
+            new_line;
+            
+            if current_selection=2 then
+                put_line("  -->   S-C-O-R-E         "); 
+                put_line("        T-A-B-L-E         "); 
+            else
+                put_line("        S-C-O-R-E         "); 
+                put_line("        T-A-B-L-E         "); 
+            end if;
+
+            new_line;
+            if current_selection=3 then
+                put_line("  -->    E-X-I-T            ");
+            else
+                put_line("         E-X-I-T            ");
+            end if;
+            
+            new_line;
+            put_line("UP- w  DOWN- s   SELECT- f");
+            put_line("--------------------------");
+           
+            delay(0.1);
+        end loop;
+        return 1;
+    end;
+end menu;
---
+++ menu.ads
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+package menu is 
+    
+    function show_menu return Integer;
+
+end menu;
