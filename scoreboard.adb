394fefe9bdb224a1cff0285101e975fa19e47e98
Milosz Blasiak <milblasiak@gmail.com>
2019-01-22 13:57:21 +0100

    Improve score board look

--- scoreboard.adb
+++ scoreboard.adb
@@ -1,7 +1,10 @@
 package body scoreboard is 
 
     procedure show_score_board is
+    keyCode : Character;
+    WasPressed : Boolean:=false;  
     begin
+        Put(ASCII.ESC & "[2J");
         put_line("------------------------");
         put_line("--------H-I-G-H---------");
         put_line("------S-C-O-R-E-S-------");
@@ -10,8 +13,12 @@ package body scoreboard is
         new_line;
         
         put_line("------------------------");
+        put_line(" PRESS-ANY-KEY-TO-RETURN ");
         put_line("------------------------");
-        put_line("------------------------");
+        
+        while WasPressed=false loop
+        Ada.Text_IO.Get_Immediate (KeyCode, WasPressed);
+        end loop;
     end;
 
 
