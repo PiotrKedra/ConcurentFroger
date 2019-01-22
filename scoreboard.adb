46c7af361c3784e5541594d9aac8f5babbb9c506
Milosz Blasiak <milblasiak@gmail.com>
2019-01-22 13:43:33 +0100

    Add score_board procedure

---
+++ scoreboard.adb
@@ -0,0 +1,18 @@
+package body scoreboard is 
+
+    procedure show_score_board is
+    begin
+        put_line("------------------------");
+        put_line("--------H-I-G-H---------");
+        put_line("------S-C-O-R-E-S-------");
+        new_line;
+        new_line;
+        new_line;
+        
+        put_line("------------------------");
+        put_line("------------------------");
+        put_line("------------------------");
+    end;
+
+
+end scoreboard;
